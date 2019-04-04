d6cd9ed7a6e00e40e93cf1658797bbfbdb56ea19
hannasong <hannasong@brilliance.com.cn>
2019-04-04 23:06:05 +0800

    添加Controller使用；

---
+++ src/main/java/com/hns/learn/web/FirstController.java
@@ -0,0 +1,18 @@
+package com.hns.learn.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class FirstController {
+
+    @RequestMapping(value={"/hello","/hi"})
+    public String say(){
+        //http://localhost:10336/hi
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(new Date());
+    }
+
+}
