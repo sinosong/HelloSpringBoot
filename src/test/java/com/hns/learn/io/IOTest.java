f14364a06fcaf46b056e474863620e176fd72a36
hannasong <watchns@163.com>
2019-09-27 09:16:16 +0800

    添加IO测试类;

---
+++ src/test/java/com/hns/learn/io/IOTest.java
@@ -0,0 +1,20 @@
+package com.hns.learn.io;
+
+import org.junit.Test;
+
+/**
+ * @author : hannasong
+ * @version : v1.0
+ * @date : Created in 9:15 上午 2019/9/27
+ * @description : 多线程io测试类;
+ */
+public class IOTest {
+
+
+    @Test
+    public void test01() {
+
+        
+
+    }
+}
